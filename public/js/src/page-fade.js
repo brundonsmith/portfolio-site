3d5ee07c574f9f5913df06f5dc19bdaa8fceaea4
Brandon Smith <brandon.smith.945@gmail.com>
2016-02-23 17:25:03 -0600

    Mobile changes and added footer

--- public/js/src/page-fade.js
+++ public/js/src/page-fade.js
@@ -3,17 +3,23 @@ for(var i = 0; i < fadeLinks.length; i++){
 	var elem = fadeLinks[i];
 	elem.onclick = function(){// Don't go to the next page yet.
 		event.preventDefault();
+		var linkLocation;
+		var openInNewTab = this.getAttribute('target') === '_blank';
+
 		if(typeof this.href !== 'undefined') {
-			var linkLocation = this.href;
+			linkLocation = this.href;
 		} else {
-			var linkLocation = this.getAttribute('data-url');
+			linkLocation = this.getAttribute('data-url');
 		}
 
-		document.getElementsByTagName('body')[0].classList.add('fadeout');
-		setTimeout(function(){
-			window.location = linkLocation;
-		}, 500);
-
+		if(openInNewTab) {
+			window.open(linkLocation);
+		} else {
+			document.getElementsByTagName('body')[0].classList.add('fadeout');
+			setTimeout(function(){
+				window.location = linkLocation;
+			}, 500);
+		}
 		return false;
 	};
 }
