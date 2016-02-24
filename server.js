3622e37e6d4c283c4f678297e416218c6bdc1c3f
Brandon Smith <brandon.smith.945@gmail.com>
2016-02-23 18:43:58 -0600

    Added contact form functionality

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var swig = require('swig');
+var nodemailer = require('nodemailer');
+var directTransport = require('nodemailer-direct-transport');
 /*
 var mongoose = require('mongoose');
 */
@@ -26,19 +28,37 @@ app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('view cache', false);
 
+// email
+var transporter = nodemailer.createTransport(directTransport({}));
 
 // pages
 app.get('/', function(request, response) {
-  response.render('index.html');
+	response.render('index.html');
 });
 app.get('/projects', function(request, response) {
-  response.render('projects.html');
+	response.render('projects.html');
 });
 app.get('/contact', function(request, response) {
-  response.render('contact.html');
+	response.render('contact.html');
 });
 
+app.post('/contact', function(request, response) {
+	console.log(JSON.stringify(request.body));
+	var mailOptions = {
+	    from: request.body.name + ' ' + '<' + request.body.email + '>', // sender address
+	    to: 'brandon.smith.945@gmail.com', // list of receivers
+	    subject: 'Portfolio Contact', // Subject line
+	    text: request.body.message // plaintext body
+	};
 
+	transporter.sendMail(mailOptions, function(error, info){
+	    if(error){
+	        return console.log(error);
+	    }
+	    console.log('Message sent');
+		response.send();
+	});
+});
 
 
 
